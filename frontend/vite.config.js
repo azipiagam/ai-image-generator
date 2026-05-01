8b5a873f5bc59143598bd28bd03ad169b314f36f
apri0429 <apriganteng91@gmail.com>
2026-05-01 18:30:25 +0700

    fixng frotend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../../backend_ai/frontend_dist',
+    outDir: '../backend_ai/frontend_dist',
     chunkSizeWarningLimit: 1000,
   },
 })
